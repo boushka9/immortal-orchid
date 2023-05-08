5425c06cc702116e73d05f9ea2246ab54e23e1cb
Danny Boushka <boushka9@gmail.com>
2023-05-08 17:51:12 -0600

    Added asset cache in registerRoute

--- client/src-sw.js
+++ client/src-sw.js
@@ -5,6 +5,7 @@ const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+// Takes in an array of URLs to pre-cache
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
@@ -26,5 +27,23 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// Define custom strategy for caching network requests 
+registerRoute(
+  // Take in request object and return boolean indicating if the requests destination is style/script/worker
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  // Try to serve a cached response if it exists
+  new StaleWhileRevalidate({
+    // Cache storage name for assets
+    cacheName: 'asset-cache',
+    plugins: [
+      // Cache only responses w a file:// status code 0, or an HTTP status 200 OK
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60, // Max numbers of entries to cache
+        maxAgeSeconds: 30 * 24 * 60 * 60, //30 Days
+      })
+    ],
+  })
+);
